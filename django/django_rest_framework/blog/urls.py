12bc398d8ee794b0dcc76ac80fa224168eb4aa68
Masato Nitta <newmtube07@gmail.com>
2025-09-19 11:45:32 +0900

    update

--- django/django_rest_framework/blog/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from rest_framework import routers
-from .views import UserViewSet, EntryViewSet
-
-class UserInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserInfo
-        # json で出力するフィールド
-        fields = ('id', 'user_name', 'birth_day', 'age', 'created_at')
--- django/practice/practice/urls.py
+++ django/practice/practice/urls.py
@@ -1,7 +1,8 @@
-"""django_rest_framework URL Configuration
+"""
+URL configuration for practice project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
-from blog.urls import router as blog_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # blog.urlsをincludeする
-    url(r'^api/', include(blog_router.urls)),
+    path("admin/", admin.site.urls),
 ]
