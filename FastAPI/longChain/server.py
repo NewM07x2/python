0e789e48654ebe090f2e11111732f96c69979ad3
Masato Nitta <newmtube07@gmail.com>
2025-09-27 17:46:30 +0900

    add "LangChainとは", serverファイル

---
+++ FastAPI/longChain/server.py
@@ -0,0 +1,45 @@
+import os
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+from fastapi.middleware.cors import CORSMiddleware
+
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"  # ここにOpenAIのAPIキーを設定"
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
+
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai",
+)
+
+model = ChatOpenAI()
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+add_routes(
+    app,
+    prompt | model,
+    path="/joke",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
